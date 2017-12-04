259643723c0b0f30d0472d4b9b9305fe92ba14b7
Marek Cermak <macermak@redhat.com>
2017-12-04 09:58:48 +0100

    Script test

--- ci/scripts/autopatch-coverity.sh
+++ ci/scripts/autopatch-coverity.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Apply patches for coverity
+# Note: Every patch that is to be applied by this script must be prefixed cov-
 
 # Directory to cd into before applying patches
 TARGET_DIR="$1"
@@ -8,12 +9,9 @@ TARGET_DIR="$1"
 # Check environment
 [ -z "$CI_PATCH_DIR" ] && echo "ERROR: CI_PATCH_DIR must be set" && exit 1
 
-set -x
-
 pushd "$TARGET_DIR"
 for p in $CI_PATCH_DIR/cov-*
 do
-	echo "$p"
 	patch --verbose -p1 < "$p"
 done
 
