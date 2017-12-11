52a5ba16710b2421a44790399efcbe01f82f95d5
Marek Cermak <macermak@redhat.com>
2017-12-11 15:03:08 +0100

    Modification of custom fedora docker

--- travis-ci/scripts/version-from-commit.sh
+++ travis-ci/scripts/version-from-commit.sh
@@ -2,6 +2,15 @@
 
 # Parse commit message if custom Fedora version should be ran
 
-[ -z "$TRAVIS_COMMIT" ] && echo "ERROR: TRAVIS_COMMIT must be set" >&2 && exit 1
+[ -z "$TRAVIS_COMMIT_MESSAGE" ] && echo "ERROR: TRAVIS_COMMIT_MESSAGE must be set" >&2 && exit 1
 
-grep -oP '(?<=\[fedora:)(.*)(?=\])' <<< $TRAVIS_COMMIT
+VERSION=`grep -oP '(?<=\[fedora:)(.*)(?=\])' <<< $TRAVIS_COMMIT_MESSAGE`
+
+[ -e $VERSION ] && exit 0
+
+declare -a FEDORA_WHITELIST=(25 26 27 latest rawhide)
+
+for ver in ${FEDORA_WHITELIST[@]}
+do
+	[[ $ver = $VERSION ]] && echo $VERSION && exit 0
+done
