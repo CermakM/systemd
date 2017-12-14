23cc27e5c4e61fd5a83f40ce52deae208d3376bc
Marek Cermak <macermak@redhat.com>
2017-12-14 16:29:16 +0100

    Disable coverity upload and fix some issues

--- travis-ci/scripts/has-git-label.sh
+++ travis-ci/scripts/has-git-label.sh
@@ -5,6 +5,9 @@
 # Check environment
 [ -z "$TRAVIS_COMMIT_MESSAGE" ] && echo "ERROR: TRAVIS_COMMIT_MESSAGE must be set" >&2 && exit 1
 [ -z "$TRAVIS_PULL_REQUEST" ] && echo "ERROR: TRAVIS_PULL_REQUEST must be set" >&2 && exit 1
+
+[ "$TRAVIS_PULL_REQUEST" = "false" ] && echo 0 && exit 0
+
 [[ "$#" -le 0 || "$#" -gt 1 ]] && echo "ERROR: INCORRECT ARGUMENT NUMBER PROVIDED" >&2 && exit 1
 
 PR_URL="https://api.github.com/repos/systemd/systemd/issues/$TRAVIS_PULL_REQUEST"
@@ -13,7 +16,8 @@ PR_LABEL_URL="$PR_URL/labels"
 
 _has_label=0
 # Check for coverity label
-declare -a labels=$(curl -s $PR_LABEL_URL | jq -r '.[] | .name')
+declare -a labels=$(curl -s $PR_LABEL_URL | jq -r '.[] | .name' 2>/dev/null)
+
 for label in ${labels[@]}; do
 	if [[ "${label,,}" = "${1,,}" ]]; then
 		_has_label=1
