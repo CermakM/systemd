6cb6d1aefb4788e585271b194993fca9c548b614
Marek Cermak <macermak@redhat.com>
2017-12-18 17:52:34 +0100

    Stable version, removed fancy features for simple PR

--- travis-ci/scripts/coverity.sh
+++ travis-ci/scripts/coverity.sh
@@ -33,41 +33,41 @@ else
   exit 1
 fi
 
-# # Verify upload is permitted
-# AUTH_RES=`curl -s --form project="$COVERITY_SCAN_PROJECT_NAME" --form token="$COVERITY_SCAN_TOKEN" $SCAN_URL/api/upload_permitted`
-# if [ "$AUTH_RES" = "Access denied" ]; then
-#   echo -e "\033[33;1mCoverity Scan API access denied. Check COVERITY_SCAN_PROJECT_NAME and COVERITY_SCAN_TOKEN.\033[0m"
-#   exit 1
-# else
-# 	AUTH=`echo $AUTH_RES | python -c "import sys, json; print json.load(sys.stdin)['upload_permitted']"`
-#   if [ "$AUTH" = "True" ]; then
-#     echo -e "\033[33;1mCoverity Scan analysis authorized per quota.\033[0m"
-#   else
-# 	  WHEN=`echo $AUTH_RES | python -c "import sys; json; print json.load(sys.stdin)['next_upload_permitted_at']"`
-#     echo -e "\033[33;1mCoverity Scan analysis NOT authorized until $WHEN.\033[0m"
-#     exit 0
-#   fi
-# fi
+# Verify upload is permitted
+AUTH_RES=`curl -s --form project="$COVERITY_SCAN_PROJECT_NAME" --form token="$COVERITY_SCAN_TOKEN" $SCAN_URL/api/upload_permitted`
+if [ "$AUTH_RES" = "Access denied" ]; then
+  echo -e "\033[33;1mCoverity Scan API access denied. Check COVERITY_SCAN_PROJECT_NAME and COVERITY_SCAN_TOKEN.\033[0m"
+  exit 1
+else
+	AUTH=`echo $AUTH_RES | python -c "import sys, json; print json.load(sys.stdin)['upload_permitted']"`
+  if [ "$AUTH" = "True" ]; then
+    echo -e "\033[33;1mCoverity Scan analysis authorized per quota.\033[0m"
+  else
+	  WHEN=`echo $AUTH_RES | python -c "import sys; json; print json.load(sys.stdin)['next_upload_permitted_at']"`
+    echo -e "\033[33;1mCoverity Scan analysis NOT authorized until $WHEN.\033[0m"
+    exit 0
+  fi
+fi
 
-# TOOL_DIR=`find $TOOL_BASE -type d -name 'cov-analysis*'`
-# export PATH=$TOOL_DIR/bin:$PATH
+TOOL_DIR=`find $TOOL_BASE -type d -name 'cov-analysis*'`
+export PATH=$TOOL_DIR/bin:$PATH
 
-# # Disable CCACHE for cov-build to compilation units correctly
-# export CCACHE_DISABLE=1
+# Disable CCACHE for cov-build to compilation units correctly
+export CCACHE_DISABLE=1
 
-# # Build
-# echo -e "\033[33;1mRunning Coverity Scan Analysis Tool...\033[0m"
-# COV_BUILD_OPTIONS=""
-# #COV_BUILD_OPTIONS="--return-emit-failures 8 --parse-error-threshold 85"
-# RESULTS_DIR="cov-int"
-# eval "${COVERITY_SCAN_BUILD_COMMAND_PREPEND}"
-# COVERITY_UNSUPPORTED=1 cov-build --dir $RESULTS_DIR $COV_BUILD_OPTIONS sh -c "$COVERITY_SCAN_BUILD_COMMAND"
-# cov-import-scm --dir $RESULTS_DIR --scm git --log $RESULTS_DIR/scm_log.txt
+# Build
+echo -e "\033[33;1mRunning Coverity Scan Analysis Tool...\033[0m"
+COV_BUILD_OPTIONS=""
+#COV_BUILD_OPTIONS="--return-emit-failures 8 --parse-error-threshold 85"
+RESULTS_DIR="cov-int"
+eval "${COVERITY_SCAN_BUILD_COMMAND_PREPEND}"
+COVERITY_UNSUPPORTED=1 cov-build --dir $RESULTS_DIR $COV_BUILD_OPTIONS sh -c "$COVERITY_SCAN_BUILD_COMMAND"
+cov-import-scm --dir $RESULTS_DIR --scm git --log $RESULTS_DIR/scm_log.txt
 
-# if [ $? != 0 ]; then
-#   echo -e "\033[33;1mCoverity Scan Build failed: $TEXT.\033[0m"
-# 	exit 1mCoverity
-# fi
+if [ $? != 0 ]; then
+  echo -e "\033[33;1mCoverity Scan Build failed: $TEXT.\033[0m"
+	exit 1mCoverity
+fi
 
 # # Upload results
 # echo -e "\033[33;1mTarring Coverity Scan Analysis results...\033[0m"
